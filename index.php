d47e98b157b5c4dec70c189fd91c1761476272fb
Sardo Ip <sardo.ip@sardo.work>
2019-12-26 19:54:08 +0800

    Add dropzone.js and style

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
   integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
   crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/dropzone.css" integrity="sha256-0Z6mOrdLEtgqvj7tidYQnCYWG3G2GAIpatAWKhDx+VM=" crossorigin="anonymous" />
   <link rel="stylesheet" href="css/style.css" />
 </head>
 
@@ -15,14 +16,14 @@
     <div class="container">
       <h1>Harto OCR</h1>
       <div class="harto-dropzone">
-        <form action="/process.php" class="dropzone">
-          <input name="file" type="file" multiple />
-          <div class="form-group">
-            <input class="btn btn-primary" type="submit" value="上傳並處理圖片" />
+        <form method="post" action="/process.php" class="dropzone">
+          <div class="fallback">
+            <input name="file" type="file" multiple />
           </div>
         </form>
       </div>
     </div>
   </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/dropzone.js" integrity="sha256-awEOL+kdrMJU/HUksRrTVHc6GA25FvDEIJ4KaEsFfG4=" crossorigin="anonymous"></script>
 </body>
 </html>
