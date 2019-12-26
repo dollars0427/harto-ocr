4d6f00d4402d5fb880a71d889a744f8c78a6c5c0
Sardo Ip <sardo.ip.sardo.work>
2019-12-26 18:58:00 +0800

    Ready base ui

--- index.php
+++ index.php
@@ -3,22 +3,26 @@
 <head>
   <meta charset="utf-8">
   <title>Harto OCR</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet"
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+  integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+  crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css" />
 </head>
 
 <body>
-  <h1>Harto OCR</h1>
-  <div class="harto-dropzone">
-    <form action="/file-upload" class="dropzone">
-      <div class="fallback">
-        <input name="file" type="file" multiple />
+  <div class="harto">
+    <div class="container">
+      <h1>Harto OCR</h1>
+      <div class="harto-dropzone">
+        <form action="/process.php" class="dropzone">
+          <input name="file" type="file" multiple />
+          <div class="form-group">
+            <input class="btn btn-primary" type="submit" value="上傳並處理圖片" />
+          </div>
+        </form>
       </div>
-    </form>
+    </div>
   </div>
-  <script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
 </body>
-
 </html>
